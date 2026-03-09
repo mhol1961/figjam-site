7e5f24320d92ff63a3e444df7ca217dcdf61e490
mhol1961 <mhol1961@users.noreply.github.com>
2026-03-09 16:35:14 +0000

    deploy: 2a751291ed6a83b993b6ed37c7527ca876e7bd04

---
+++ _next/static/chunks/app/menu/layout-f41823ab0735332d.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[164,841,211,319,719,30,840],{3521:function(){}},function(n){n.O(0,[971,117,744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
